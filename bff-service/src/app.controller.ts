72a206a63e25608794b5f0b1932e2f4398443a8e
Gergii Mazur <gmazur92@gmail.com>
2021-11-06 20:49:42 +0200

    feat: add bff-service

---
+++ bff-service/src/app.controller.ts
@@ -0,0 +1,20 @@
+import { Controller, Req, Res, HttpException, HttpStatus, All } from '@nestjs/common';
+import type { Request, Response } from 'express';
+import { AppService } from './app.service';
+
+@Controller('/*')
+export class AppController {
+  constructor(private readonly appService: AppService) {}
+
+  @All()
+  async redirect(@Req() req: Request, @Res() res: Response): Promise<any> {
+    const { url, service } = this.appService.getRedirectPath(req.url);
+
+    if (!url) {
+      throw new HttpException('Cannot process request', HttpStatus.BAD_GATEWAY);
+    }
+
+    const result = await this.appService.sendRequest(url, service, req.method, req.body);
+    return res.status(result.status).json(result.data);
+  }
+}
---
+++ bff-service/src/app.service.ts
@@ -0,0 +1,61 @@
+import axios, { Method, AxiosRequestConfig } from 'axios';
+import { HttpStatus, CACHE_MANAGER, Injectable, Logger, Inject } from '@nestjs/common';
+import { Cache } from 'cache-manager';
+
+@Injectable()
+export class AppService {
+  constructor(@Inject(CACHE_MANAGER) private cacheManager: Cache) {
+  }
+
+  private readonly logger = new Logger(AppService.name);
+
+  getRedirectPath(originalUrl: string) {
+
+    this.logger.log(`originalUrl: ${originalUrl}`);
+    let service = originalUrl.split('/')[ 1 ];
+    this.logger.log(`service: ${service}`);
+    let params = originalUrl.split('/')[ 2 ];
+
+    if (!service || !process.env[ service ]) {
+      this.logger.warn(`${service} not found`);
+      return null;
+    }
+
+    let path = process.env[ service ]
+
+    if (params) {
+      path = `${path}/${params}`
+    }
+
+    return { url: path, service  };
+  }
+
+  async sendRequest(url: string, service: string, method: string, data: Record<string, any> = {}) {
+
+    if (service === 'products' && method === 'GET') {
+      const products = await this.cacheManager.get('products');
+      if (products) {
+        return { status: HttpStatus.OK, data: products };
+      }
+    }
+
+    const axiosConfig: AxiosRequestConfig = {
+      url,
+      method: method as Method,
+    };
+    if (Object.keys(data).length) {
+      axiosConfig.data = data;
+    }
+
+    try {
+      const { status, data } = await axios(axiosConfig);
+
+      if (service === 'products' && method === 'GET') {
+        await this.cacheManager.set('products', data, { ttl: 120 });
+      }
+      return { status, data };
+    } catch (e) {
+      return { status: e.response.status, data: e.response.data };
+    }
+  }
+}
